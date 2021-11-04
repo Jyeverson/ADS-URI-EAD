0c7d65d2a1bb49f8b5a098b8c92e17bfcc7747e2
Jyeverson <jyeversonsantos@gmail.com>
2021-11-03 21:09:52 -0300

    Abstract Classes in progress

---
+++ Programação Orientada a Objetos/Desafios/Classes Abstratas/ClassesAbstratas/src/main/java/Cadastro/Fisica.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cadastro;
+
+/**
+ *
+ * @author jyeverson.madalozo
+ */
+public class Fisica {
+    String nome,cpf,rg,idade,titulacao;
+}
---
+++ Programação Orientada a Objetos/Desafios/Classes Abstratas/ClassesAbstratas/src/main/java/Cadastro/Pessoa.java
@@ -0,0 +1,11 @@
+package Cadastro;
+
+/**
+ *
+ * @author jyeverson.madalozo
+ */
+public class Pessoa {
+    String numero,bairro,cidade,uf,telefone,email,disciplina,logradouro;
+
+    
+}
